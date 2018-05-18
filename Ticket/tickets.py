c845c689ffffc724d4e39a81453bb06dbde9bcff
rgdsmint <rgdsmint@gmail.com>
2018-05-18 18:09:03 +0800

    修改得更完善一些

--- Ticket/tickets.py
+++ Ticket/tickets.py
@@ -1,4 +1,4 @@
-"""
+'''
 Usage:
     ticket [-dgktz] <from> <to> <date>
 
@@ -15,25 +15,47 @@ Examples：
     Please input the city you want leave :南京
     Please input the city you will arrive :北京
     Please input the date(Example:2017-09-27) :2018-03-01
-"""
+'''
 from docopt import docopt
 import requests
-from stations import station_code, code_station
+from stations import station_code, code_station, stationsChineseName
 from prettytable import PrettyTable
+import time
 
 '''
 查票程序
 使用方法如上
 '''
+
+
 class SearchTicket:
 
     # 接受所查票的参数 : 车票类型 起始车站 日期
     def __init__(self):
-        self.trainOption = input('-d动车 -g高铁 -k快速 -t特快 -z直达\n请输入您要查询的车次类型(如:-dg) :')
-        self.fromStation = input('请输入您出发的城市(如:滁州) :')
-        self.toStation = input('请输入您要前往的城市(如:上海) :')
-        self.Date = input('请输入日期(如:2018-05-09) :')
+        self.trainOption = input('-d动车 -g高铁 -k快速 -t特快 -z直达\n请输入您要查询的车次类型(如:-dg PS:默认全选) :')
+        # 如果没有填参数或者参数错误 则按默认进行
+        if len(self.trainOption) == 0 or self.trainOption not in '-dgktz' and len(self.trainOption) != 0:
+            self.trainOption = '-dgktz'  # 默认全选 (-dgktz)
+            print('\t输入有误 已按默认类型执行')
+
+        self.fromStation = input('请输入您出发的城市(如:南京 PS:默认南京) :')
+        # 如果参数错误(不在站点名称里面 则按默认进行)
+        if self.fromStation not in stationsChineseName:
+            self.fromStation = '南京' # 默认南京
+            print('\t输入有误 已按默认起始点执行')
+
+        self.toStation = input('请输入您要前往的城市(如:上海 PS:默认上海 :')
+        # 如果参数错误(不在站点名称里面 则按默认进行)
+        if self.toStation not in stationsChineseName:
+            self.toStation = '上海' # 默认上海
+            print('\t输入有误 已按默认终止点执行')
+
+        self.Date = input('请输入日期(如:2018-05-09 PS:默认日期为当天) :')
+        if len(self.Date) == 0 or len(self.Date) != 10:
+            self.Date = time.strftime('%Y-%m-%d')  # 默认日期为当天
+            print('\t输入有误 已按默认时间执行')
         self.train = self.trains()
+
     # 处理信息  得出官网信息  并写成列表
     def searchTrain(self):
         arguments = {
@@ -48,12 +70,13 @@ leftTicketDTO.train_date={}\
 &leftTicketDTO.from_station={}&\
 leftTicketDTO.to_station={}\
 &purpose_codes=ADULT'.format(arguments['date'], arguments['from_station'], arguments['to_station'])
-        requests.packages.urllib3.disable_warnings()
+        requests.urllib3.disable_warnings()
         r = requests.get(url, verify=False)
         try:
             self.raw_trains = r.json()['data']['result']
         except:
             exit('输入有误')
+
     # 解析信息  得出各参数
     def trains(self):
         for item in self.raw_trains:
@@ -98,6 +121,7 @@ leftTicketDTO.to_station={}\
                     other_seat
                 ]
                 yield train
+
     # 将参数可视化
     def pretty_print(self):
         pt = PrettyTable()
@@ -106,6 +130,8 @@ leftTicketDTO.to_station={}\
         for train in self.train:
             pt.add_row(train)
         print(pt)
+
+
 # 运行程序
 # 增加循环以重复查询
 if __name__ == '__main__':
@@ -113,3 +139,8 @@ if __name__ == '__main__':
         st = SearchTicket()
         st.searchTrain()
         st.pretty_print()
+        # 询问是否继续
+        message = input('是否继续?[y/n] PS:默认为n')
+        # 如果不是'y' 则退出
+        if message == 'n' or len(message) == 0 or message != 'y':
+            exit('程序已结束')
